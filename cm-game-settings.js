2c0e48ff8cfc05b521b2ab8cb07a07f8eaf37403
zeel <zeel02@gmail.com>
2021-01-05 01:20:09 -0500

    Refactored settings.

--- cm-game-settings.js
+++ cm-game-settings.js
@@ -2,51 +2,21 @@
 Hooks.once("init", function () {
 	const namespace ="_CodeMirror";
 
-	const name = function(setting) {
-		return game.i18n.localize(`${namespace}.settings.${setting}.name`);
-	}
-	const hint = function(setting) {
-		return game.i18n.localize(`${namespace}.settings.${setting}.hint`);
+	const register = function(name, type, def) {
+		game.settings.register(namespace, name, {
+			name: game.i18n.localize(`${namespace}.settings.${name}.name`),
+			hint: game.i18n.localize(`${namespace}.settings.${name}.hint`),
+			scope: "world",
+			config: true,
+			type: type,
+			default: def
+		});
 	}
 
-	game.settings.register(namespace, "smartIndent", {
-		name: name("smartIndent"),		
-		hint: hint("smartIndent"),
-		scope: "world",
-		config: true,
-		type: Boolean,
-		default: true
-	});
-	game.settings.register(namespace, "indentUnit", {
-		name: name("indentUnit"),		
-		hint: hint("indentUnit"),
-		scope: "world",
-		config: true,
-		type: Number,
-		default: 4
-	});
-	game.settings.register(namespace, "tabSize", {
-		name: name("tabSize"),		
-		hint: hint("tabSize"),
-		scope: "world",
-		config: true,
-		type: Number,
-		default: 4
-	});
-	game.settings.register(namespace, "indentWithTabs", {
-		name: name("indentWithTabs"),		
-		hint: hint("indentWithTabs"),
-		scope: "world",
-		config: true,
-		type: Boolean,
-		default: true
-	});
-	game.settings.register(namespace, "lineWrapping", {
-		name: name("lineWrapping"),		
-		hint: hint("lineWrapping"),
-		scope: "world",
-		config: true,
-		type: Boolean,
-		default: false
-	});
+	register("smartIndent"   , Boolean,  true);
+	register("indentUnit"    , Number ,     4);
+	register("tabSize"       , Number ,     4);
+	register("indentWithTabs", Boolean,  true);
+	register("smartIndent"   , Boolean,  true);
+	register("lineWrapping"  , Boolean, false);
 })
